c41c8d9003d11356653ca29a286d77626db09865
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-02-24 23:46:42 +0500

    Забыл сам slack.py

---
+++ carbon_monitoring/slack.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+""" отправка в чятик slack """
+import os
+import json
+from urllib2 import urlopen
+
+def slack(val, string):
+    """ всего-то отправить текст urlopen'ом """
+    HOOK = os.environ.get('SLACK_HOOK_URL')
+    urlopen(HOOK, data=json.dumps({"text": string,}))
